5e7600d40702d868ae50c6c85a45ddfbf96235f8
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-25 23:16:52 +0600

    initial_state

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def printBoard():
+    print(f"0 | 1 | 2")
+    print(f"--|--|--")
+    print(f"3 | 4 | 5")
+    print(f"--|--|--")
+    print(f"6 | 7 | 8")
+    pass
 
 if __name__ == "__main__":
     xState = [0,0,0,0,0,0,0,0]
     zState = [0,0,0,0,0,0,0,0]
     turn = 1 
     print("Welcome to tic tac toe ")
+    while(True):
+        if(turn == 1)
+            print()
+    
