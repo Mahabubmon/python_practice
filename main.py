4411aa045863a8bedbed77cc83fc2f9db16e6a5f
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-27 23:27:24 +0600

    math_

--- main.py
+++ main.py
@@ -4,8 +4,15 @@
 # age = birth_year - current_year
 # print (age)
 
-first_name = "Md " 
-middle_name = "Mahabubur "
-last_name = "Rahman"
-print(first_name +'' +  middle_name  + '' + last_name)
+# first_name = "Md " 
+# middle_name = "Mahabubur "
+# last_name = "Rahman"
+# print(first_name +'' +  middle_name  + '' + last_name)
 
+base = 10.2
+height = 20.5
+
+area = (1/2)*base*height
+print(area)
+
+round(area)
