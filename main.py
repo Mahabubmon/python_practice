11e902842aca3b8704bd22b23e270bd94c336410
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-27 23:20:09 +0600

    variables

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-birth_year = 1956
-current_year = 2023
+# birth_year = 1956
+# current_year = 2023
 
-age = birth_year - current_year
-print (age)
+# age = birth_year - current_year
+# print (age)
+
+first_name = "Md " 
+middle_name = "Mahabubur "
+last_name = "Rahman"
+print(first_name +'' +  middle_name  + '' + last_name)
+
