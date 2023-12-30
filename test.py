e2a6c10a9cdd7c672b7582d724d428434dfbcc78
Mahabubmon <mahabubmon@gmail.com>
2023-12-30 17:46:03 +0600

    modify string

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 # for x in "banana":
 #     print(x) 
 
-text = "this is a goal"
+#text = "this is a goal"
 
-if "goal" in text:
-    print("yes,it is on it")
-    print("expensive" not in text)
+#if "goal" in text:
+   # print("yes,it is on it")
+   # print("expensive" not in text)
 
 
-txt = "life is free"
-if "expensive" not in txt:
-    print("expensive, Not in the Sentence")
+#txt = "life is free"
+#if "expensive" not in txt:
+#   print("expensive, Not in the Sentence")
+
+a = "Hellow, World! "
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace("H","J"))
+print(a.split())
+
+
