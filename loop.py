75daf0edf28a92157dd00ff30442515edfe64bd4
Mahabubur Rahman <mahabubmon@gmail.com>
2023-12-31 00:00:10 +0600

    __

--- loop.py
+++ loop.py
@@ -1,19 +1,26 @@
-road = ["posta","islambagh","lalbagh"]
-for i in road:
-    print(i)
+# road = ["posta","islambagh","lalbagh"]
+# for i in road:
+#     print(i)
 
 
 place = ["azimpur","new_market","dhanmondi"]
-for x in range(len(place)):
-    print(place[x])
+# for x in range(len(place)):
+#     print(place[x])
 
 
 
-i = 0 
-while i < len(place):
-    print(place[i])
-    i = i + 1
+# i = 0 
+# while i < len(place):
+#     print(place[i])
+#     i = i + 1
 
 
 
-[print(x) for x in place]
+# [print(x) for x in place]
+
+newlist = []
+for x in place:
+    if "a" in x:
+        newlist.append(x)
+
+        print(newlist)
