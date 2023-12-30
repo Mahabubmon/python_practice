5785f5ce9d42e5b299dfad356b9e5124de34386e
Mahabubmon <mahabubmon@gmail.com>
2023-12-30 17:36:03 +0600

    python_strings

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-for x in "banana":
-    print(x) 
+# for x in "banana":
+#     print(x) 
+
+text = "this is a goal"
+
+if "goal" in text:
+    print("yes,it is on it")
+    print("expensive" not in text)
+
+
+txt = "life is free"
+if "expensive" not in txt:
+    print("expensive, Not in the Sentence")
