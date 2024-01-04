a79367c3b479d7a05e8eb5d2808faee044a7ab11
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-04 23:42:02 +0600

    function

---
+++ function.py
@@ -0,0 +1,10 @@
+def my_function(fname, lname):
+    print(fname+" "+lname)
+
+my_function("Rahul","Arif")
+
+
+def my_function2(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
