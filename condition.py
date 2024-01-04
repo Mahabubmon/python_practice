836a6eb3b4b00bf4ee1b93e52dda64adc5068c53
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-04 23:12:43 +0600

    condition

---
+++ condition.py
@@ -0,0 +1,16 @@
+a = 33 
+b = 33
+if b > a:
+    print("b is grater than a")
+elif a==b:
+    print("a and b are equal")
+
+#preceding conditions
+
+c = 300
+if b > c:
+    print("b is grater than c ")
+elif a == c:
+    print("a and c are equal")
+else:
+    print("c is gater than all of them")
