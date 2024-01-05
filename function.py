69efa0fb98ce7762c1557657eaec47783c4de28d
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-05 21:56:33 +0600

    list as a argument

--- function.py
+++ function.py
@@ -1,10 +1,28 @@
-def my_function(fname, lname):
-    print(fname+" "+lname)
+# def my_function(fname, lname):
+#     print(fname+" "+lname)
 
-my_function("Rahul","Arif")
+# my_function("Rahul","Arif")
 
 
-def my_function2(child3, child2, child1):
-  print("The youngest child is " + child3)
+# def my_function2(child3, child2, child1):
+#   print("The youngest child is " + child3)
 
-my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+# my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
