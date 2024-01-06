b5c4131018e6a3ea2d6bb17c93e5b4d21e11e982
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-06 22:07:04 +0600

    add_function

--- class.py
+++ class.py
@@ -1,7 +1,7 @@
-# class Person:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
 
 # p1 = Person("John", 36)
@@ -26,15 +26,24 @@
 # p1.myfunc()
 
 
-class Person:
-    def __init__(mysillyobject, name, age):
-        mysillyobject.name = name
-        mysillyobject.age = age
+# class Person:
+#     def __init__(mysillyobject, name, age):
+#         mysillyobject.name = name
+#         mysillyobject.age = age
 
 
-    def myfunc(abc):
-        print("Hello my name is " + abc.name)
+#     def myfunc(abc):
+#         print("Hello my name is " + abc.name)
 
 
-p1 = Person("Arif", 24)
-p1.myfunc()
+# p1 = Person("Arif", 24)
+# p1.myfunc()
+
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
