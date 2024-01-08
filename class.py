74e0456949e6331f105460748479cde715175c8e
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-08 14:15:29 +0600

    stop_iterations

--- class.py
+++ class.py
@@ -48,13 +48,31 @@ class Person:
 #   def welcome(self):
 #     print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
 
-class Student(Person):
-    def __init__(self, fname, lname, year):
-        super().__init__(fname, lname, year)
-        self.graduationyear = year
+# class Student(Person):
+#     def __init__(self, fname, lname, year):
+#         super().__init__(fname, lname, year)
+#         self.graduationyear = year
 
-# x = Student("Rahul","Al", 2019)
+# # x = Student("Rahul","Al", 2019)
         
-    def welcome(self):
-      print("Welcome", self.fname, self.lname,"to the class of",
-            self.graduationyear)
+#     def welcome(self):
+#       print("Welcome", self.fname, self.lname,"to the class of",
+#             self.graduationyear)
+        
+class Mynumber:
+    def __iter__(self):
+        self.a = 1
+        return self
+    
+    def __next__(self):
+        if self.a <= 20:
+            self.a += 1 
+            return x 
+        else:
+            raise StopIteration
+        
+myclass = Mynumber()
+myiter = iter(myclass)
+
+for x in myiter:
+    print(x)
