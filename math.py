4280108b4d2807ca659e25f6542f96323f26ecec
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-09 23:09:03 +0600

    math

--- math.py
+++ math.py
@@ -1,11 +1,27 @@
+import math
+
 x = min("5","10","25")
+
 y = max("5","10","25")
 
 z = abs(-7.25)
 
 print(z)
 
+a = pow(4, 3)
 
 
 print(x)
 print(y)
+
+print(a)
+
+
+b = math.sqrt(64)
+print(b)
+
+c = math.ceil(2.5)
+d = math.floor(2.5)
+
+print(c)
+print(d)
